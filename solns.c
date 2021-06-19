e64d068b758383397b191bcf93b07598ae7cc67c
Esth-star <84137912+Esth-star@users.noreply.github.com>
2021-06-19 23:03:12 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -4,14 +4,15 @@
 
 /*max*/
   int max(int a[], int n)
-  {
+{
     int max = 0;
-    for(int i=0; i<n; i++){
+    for(int i=0; i<n; i++)
+    {
       if(a[i]>max)
         max= a[i];
     } return max;
     
-  }
+}
   
   /*min*/
   int min(int a[], int n)
@@ -28,44 +29,45 @@
     
     
      float average(int arr[], int n)
+     {
     float sum = 0;
-float avg =0;
+    float avg =0;
       for(int i=o; i<n; i++)
-      {
+     {
           sum = sum+arr[i];
-      }
+     }
        avg =sum/n;
      return avg;
-    }
+     }
   
   
   /*mode*/
   
   int mode(int arr[], int n)
     
-     {
+{
    int i,j,cnt = 0,count = 0,max=0;
 
    for (i=0; i<n; i++)
    {
       
-    for (j=0; j<n; j++) {
+   for (j=0; j<n; j++) 
+   {
          if (arr[i] == arr[j]){
          cnt++;
-      }
-      }
-      if (cnt>=max)
-      {
+   }
+   }
+  if (cnt>=max)
+   {
          count = max;
          max = arr[i];
-      }
+   }
    }
 
    return max;
-}
-    
-       int factors(int n, int arr[])
-       {
+} 
+int factors(int n, int arr[])
+{
          int i,j=0;
          for(i=2; i<=n; i++)
            
@@ -78,7 +80,7 @@ float avg =0;
            }
          }
          return j;
-       }
+}
           
       
       
